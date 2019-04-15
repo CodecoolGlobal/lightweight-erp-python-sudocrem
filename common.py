39aae1e38584bbb63cd064d869bde7b5886082d7
Panna Kristóf <kristofpanna@gmail.com>
2019-04-15 17:59:18 +0200

    implement generate_random (task: 2)

--- common.py
+++ common.py
@@ -5,6 +5,10 @@ implement commonly used functions here
 import random
 
 
+def get_random_char_between(letter_1, letter_2):
+    return chr(random.randint(ord(letter_1), ord(letter_2)))
+
+
 def generate_random(table):
     """
     Generates random and unique string. Used for id/key generation:
@@ -18,8 +22,18 @@ def generate_random(table):
         string: Random and unique string
     """
 
-    generated = ''
+    keys = [row[0] for row in table]
 
-    # your code
+    generated_in_keys = True
+    while generated_in_keys:
+        generated = ''
+        for _ in range(2):
+            generated += get_random_char_between("a", "z")
+            generated += get_random_char_between("A", "Z")
+            generated += get_random_char_between("!", "/")
+            generated += get_random_char_between("0", "9")
+
+        if not (generated in keys):
+            generated_in_keys = False
 
     return generated
