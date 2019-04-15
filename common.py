774a9f905c46f36cf3af647107895890fa4e0a79
verho <verhoczki.marci@gmail.com>
2019-04-15 22:22:58 +0200

    Add get_longest_elements to commin, ID:9

--- common.py
+++ common.py
@@ -23,3 +23,24 @@ def generate_random(table):
     # your code
 
     return generated
+
+
+def get_longest_elements(table):
+    """
+    Returns a list of integers, containing the length of the longest element in a column.
+    Looks awful, but works...
+    """
+    columns = len(table[0])
+    longest_elements = [0] * columns
+    for column in range(columns):
+        max_column_length = 0
+        for line in table:
+            column_element = line[column]
+            if len(column_element) > max_column_length:
+                max_column_length = len(column_element)
+        longest_elements[column] = max_column_length
+    return longest_elements
+
+
+
+
