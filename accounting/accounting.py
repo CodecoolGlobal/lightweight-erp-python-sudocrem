0eac88622cac09fc1f8b579d635beb8e432759d8
Fanni Nagy <nagy.fanni.5@gmail.com>
2019-04-17 11:23:54 +0200

    add special which_year_max function

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -35,6 +35,7 @@ def start_module():
     (2): Add to table
     (3): Remove from table
     (4): Update table
+    (5): Year of highest profit
     """
     file_name = 'accounting/items.csv'
     table = data_manager.get_table_from_file("accounting/items.csv")
@@ -42,7 +43,8 @@ def start_module():
         "Show table",
         "Add to table",
         "Remove from table",
-        "Update table"]
+        "Update table",
+        "Year of highest profit"]
     while True:
         ui.print_menu("Accounting menu:", accounting_options, "Go back to main menu:")
         inputs = ui.get_inputs(["Please enter a number: "], "")
@@ -59,6 +61,9 @@ def start_module():
             get_id = ui.get_inputs(["Please enter an ID to update: "], "")
             id_ = get_id[0]
             update(table, id_)
+        elif option == "5":
+            year_max = which_year_max(table)
+            ui.print_result(str(year_max), "Year of highest profit")
         elif option == "0":
             return None
         else:
@@ -76,9 +81,9 @@ def show_table(table):
     Returns:
         None
     """
-    title_list = ['ID', 'Month', 'Day', 'Year', 'Type', 'Amount']
-    common.show(table, title_list)
     # your code
+    title_list = ['ID', 'Month', 'Day', 'Year', 'Type', 'Amount']
+    common.show_table(table, title_list)
 
 
 def add(table):
@@ -92,7 +97,7 @@ def add(table):
         list: Table with a new record
     """
     # your code
-    title_list = ['ID', 'Month', 'Day', 'Year', 'Type', 'Amount']
+    title_list = ['Month', 'Day', 'Year', 'Type', 'Amount']
     return common.add(table, title_list)
 
 
@@ -125,7 +130,7 @@ def update(table, id_):
     """
 
     # your code
-    title_list = ['ID', 'Month', 'Day', 'Year', 'Type', 'Amount']
+    title_list = ['Month', 'Day', 'Year', 'Type', 'Amount']
     return common.update(table, id_, title_list)
 
 
@@ -135,15 +140,25 @@ def update(table, id_):
 def which_year_max(table):
     """
     Question: Which year has the highest profit? (profit = in - out)
-
     Args:
         table (list): data table to work on
-
     Returns:
         number
     """
-
     # your code
+    # type (string): in = income, out = outflow
+
+    data_year = 3
+    data_type = 4
+    data_amount = 5
+    profit_max = 0
+    year_max = 0
+
+    for data in table:
+        if (data[data_type] == "in") and (int(data[data_amount]) > profit_max):
+            profit_max = int(data[data_amount])
+            year_max = data[data_year]
+    return year_max
 
 
 def avg_amount(table, year):
